9d89fd4fe9f083954e089fcb8e59401a33acd95c
hyuckbin <rpdak159@naver.com>
2025-10-17 10:59:00 +0900

    axios 설치

--- bunge-market/src/pages/MainPage.jsx
+++ bunge-market/src/pages/MainPage.jsx
@@ -1,20 +1,34 @@
 import { Container, Grid, Typography, Button } from "@mui/material";
+import axios from "axios";
+
+import { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
 
 export default function MainPage() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://68db330023ebc87faa323a6c.mockapi.io/bunge")
+      .then((res) => setProducts(res.data))
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
     <Container sx={{ mt: 4 }}>
       <Typography variant="h4" gutterBottom>
         상품 목록
       </Typography>
-      <Button variant="contained" color="secondary" sx={{ mb: 2 }}>
-        최신순 정렬
-      </Button>
+
       <Grid container spacing={3}>
-        {/* 임시 데이터 예시 */}
-        {[1, 2, 3].map((item) => (
-          <Grid item xs={12} sm={6} md={4} key={item}>
-            <ProductCard />
+        {products.map((p) => (
+          <Grid item xs={12} sm={6} md={4} key={p.id}>
+            <ProductCard
+              title={p.title}
+              price={p.price}
+              location={p.location}
+              image={p.image}
+            />
           </Grid>
         ))}
       </Grid>
