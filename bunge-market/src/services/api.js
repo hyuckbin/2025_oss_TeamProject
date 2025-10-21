91186023dfbd00337fbc39bf21a3355cc5620918
junhyoungSon <junhyoung337@handong.ac.kr>
2025-10-21 22:02:22 +0900

    디테일 페이지를 못만들겠다

--- bunge-market/src/services/api.js
+++ bunge-market/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // MockAPI URL (고혁빈님한테 받아서 여기에 입력하세요!)
-const API_BASE_URL = 'https://68db330023ebc87faa323a6c.mockapi.io/bunge';
+const API_BASE_URL = 'https://68db330023ebc87faa323a6c.mockapi.io';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
@@ -12,31 +12,31 @@ const api = axios.create({
 });
 
 // 전체 상품 조회
-export const getProducts = async () => {
-  const response = await api.get('/products');
+export const getbunge = async () => {
+  const response = await api.get('/bunge');
   return response.data;
 };
 
 // 단일 상품 조회
 export const getProduct = async (id) => {
-  const response = await api.get(`/products/${id}`);
+  const response = await api.get(`/bunge/${id}`);
   return response.data;
 };
 
 // 상품 등록
 export const createProduct = async (productData) => {
-  const response = await api.post('/products', productData);
+  const response = await api.post('/bunge', productData);
   return response.data;
 };
 
 // 상품 수정
 export const updateProduct = async (id, productData) => {
-  const response = await api.put(`/products/${id}`, productData);
+  const response = await api.put(`/bunge/${id}`, productData);
   return response.data;
 };
 
 // 상품 삭제
 export const deleteProduct = async (id) => {
-  const response = await api.delete(`/products/${id}`);
+  const response = await api.delete(`/bunge/${id}`);
   return response.data;
 };
